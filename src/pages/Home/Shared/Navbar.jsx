c17a957ac50603d5c72a5e6694ed872d831b07f7
tanvirahmedtagim <tanvirahmedtagim@gmail.com>
2025-01-02 22:54:31 +0600

    Popular Menu work done

--- src/pages/Home/Shared/Navbar.jsx
+++ src/pages/Home/Shared/Navbar.jsx
@@ -12,8 +12,8 @@ const Navbar = () => {
     </>
   );
   return (
-    <div>
-      <div className="navbar z-10 fixed bg-opacity-35 bg-black ">
+    <div className="">
+      <div className="navbar z-10 fixed bg-opacity-35 bg-black">
         <div className="navbar-start">
           <div className="dropdown">
             <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
